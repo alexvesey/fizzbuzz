61ed2f4d9d924b1655628d6886b7597f5ff92d29
Alex <alex@DESKTOP-PFUV74T.localdomain>
2019-08-05 20:13:46 -0400

    Fizzbuzz V2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 fn main() {
     for i in 0..101 {
-        if (i % 3 == 0) {
-            print!("Fizz");
-        }
-        if (i % 5 == 0) {
-            print!("Buzz");
-        }
-        else if (i % 3 !=0 && i % 5 != 0) {
-            print!("{}", i.to_string());
-        }
-        println!();
+        let mut output = String::new();
+        if ( i % 3 == 0) { output += "Fizz"}
+        if ( i % 5 == 0) { output += "Buzz"}
+        if (output == "") {output += &i.to_string()}
+
+        println!("{}", output);
     }
 }
